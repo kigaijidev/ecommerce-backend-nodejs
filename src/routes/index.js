376f5e915fc273359d3694480477694569c957c0
Kigaiji <chminh19th2@gmail.com>
2023-10-04 22:15:21 +0700

    add middleware check permission

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,14 @@
 'use strict'
 
 const express = require('express')
-const { apiKey } = require('../auth/checkAuth')
+const { apiKey, permission } = require('../auth/checkAuth')
 const router = express.Router()
 
 // check apiKey
 router.use(apiKey)
 // check permission
+router.use(permission('0000'))
 
 router.use('/v1/api', require('./access'))
-// router.use('/', ( req, res, next) => {
-//     return res.status(200).send('Hello NodeJs')
-// })
 
 module.exports = router
